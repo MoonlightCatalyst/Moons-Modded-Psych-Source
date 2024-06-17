493c31d3c2dfbff6b24907c1d66ebe6fa25e21a5
Pumpsuki <71671417+PartnerCapPikachu@users.noreply.github.com>
2024-06-17 00:35:29 -0400

    Update CoolUtil.hx

--- source/backend/CoolUtil.hx
+++ source/backend/CoolUtil.hx
@@ -3,107 +3,75 @@ package backend;
 import openfl.utils.Assets;
 import lime.utils.Assets as LimeAssets;
 
-class CoolUtil
-{
-	inline public static function quantize(f:Float, snap:Float){
-		// changed so this actually works lol
-		var m:Float = Math.fround(f * snap);
-		//trace(snap);
-		return (m / snap);
+class CoolUtil {
+
+	inline public static function quantize(f:Float, snap:Float):Float {
+		return Math.fround(f * snap) / snap;
 	}
 
-	inline public static function capitalize(text:String)
+	inline public static function capitalize(text:String):String {
 		return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
+	}
 
-	inline public static function coolTextFile(path:String):Array<String>
-	{
+	inline public static function coolTextFile(path:String):Array<String> {
 		var daList:String = null;
-		#if (sys && MODS_ALLOWED)
-		var formatted:Array<String> = path.split(':'); //prevent "shared:", "preload:" and other library names on file path
-		path = formatted[formatted.length-1];
-		if(FileSystem.exists(path)) daList = File.getContent(path);
-		#else
-		if(Assets.exists(path)) daList = Assets.getText(path);
+		#if (sys && MODS_ALLOWED) if (FileSystem.exists(path)) {
+			daList = File.getContent(path);
+		} #else if (Assets.exists(path)) {
+			daList = Assets.getText(path);
+		}
 		#end
 		return daList != null ? listFromString(daList) : [];
 	}
 
-	inline public static function colorFromString(color:String):FlxColor
-	{
-		var hideChars = ~/[\t\n\r]/;
-		var color:String = hideChars.split(color).join('').trim();
-		if(color.startsWith('0x')) color = color.substring(color.length - 6);
-
-		var colorNum:Null<FlxColor> = FlxColor.fromString(color);
-		if(colorNum == null) colorNum = FlxColor.fromString('#$color');
-		return colorNum != null ? colorNum : FlxColor.WHITE;
+	inline public static function colorFromString(color:String):FlxColor {
+		color = ~/[\t\n\r]/.split(color).join('').trim();
+		if (color.startsWith('0x')) {
+			color = color.substring(color.length - 6);
+		}
+		return (FlxColor.fromString(color) ?? FlxColor.fromString('#$color')) ?? FlxColor.WHITE;
 	}
 
-	inline public static function listFromString(string:String):Array<String>
-	{
-		var daList:Array<String> = [];
-		daList = string.trim().split('\n');
-
-		for (i in 0...daList.length)
-			daList[i] = daList[i].trim();
-
-		return daList;
+	inline public static function listFromString(string:String):Array<String> {
+		return string.trim().split('\n').map((i:String) -> {i.trim();});
 	}
 
-	public static function floorDecimal(value:Float, decimals:Int):Float
-	{
-		if(decimals < 1)
-			return Math.floor(value);
-
-		var tempMult:Float = 1;
-		for (i in 0...decimals)
-			tempMult *= 10;
-
-		var newValue:Float = Math.floor(value * tempMult);
-		return newValue / tempMult;
+	public static function floorDecimal(value:Float, decimals:Int):Float {
+		final tempMult:Float = Math.pow(10, decimals);
+		return decimals < 1 ? Math.floor(value) : Math.floor(value * tempMult) / tempMult;
 	}
 
-	inline public static function dominantColor(sprite:flixel.FlxSprite):Int
-	{
-		var countByColor:Map<Int, Int> = [];
-		for(col in 0...sprite.frameWidth) {
-			for(row in 0...sprite.frameHeight) {
-				var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
-				if(colorOfThisPixel != 0) {
-					if(countByColor.exists(colorOfThisPixel))
-						countByColor[colorOfThisPixel] = countByColor[colorOfThisPixel] + 1;
-					else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687))
-						countByColor[colorOfThisPixel] = 1;
+	inline public static function dominantColor(sprite:flixel.FlxSprite):Int {
+		var countByColor:Map<Int,Int> = [];
+		for (col in 0...sprite.frameWidth) {
+			for (row in 0...sprite.frameHeight) {
+				var pixClr:Int = sprite.pixels.getPixel32(col, row);
+				if (pixClr != 0) {
+					if (countByColor.exists(pixClr)) {
+						countByColor[pixClr]++;
+					} else if (countByColor[pixClr] != -13520687) {
+						countByColor[pixClr] = 1;
+					}
 				}
 			}
 		}
-
-		var maxCount = 0;
-		var maxKey:Int = 0; //after the loop this will store the max color
+		var maxCount:Int = 0;
+		var maxKey:Int = 0;
 		countByColor[FlxColor.BLACK] = 0;
-		for(key in countByColor.keys()) {
-			if(countByColor[key] >= maxCount) {
-				maxCount = countByColor[key];
-				maxKey = key;
+		for (key in countByColor.keys()) {
+			if (countByColor[key] >= maxCount) {
+				maxCount = countByColor[maxKey = key];
 			}
 		}
 		countByColor = [];
 		return maxKey;
 	}
 
-	inline public static function numberArray(max:Int, ?min = 0):Array<Int>
-	{
-		var dumbArray:Array<Int> = [];
-		for (i in min...max) dumbArray.push(i);
-
-		return dumbArray;
+	inline public static function numberArray(max:Int, ?min:Int = 0):Array<Int> {
+		return [for (i in min...max) {i;}];
 	}
 
-	inline public static function boundTo(value:Float, min:Float, max:Float):Float {
-		return Math.max(min, Math.min(max, value));
-	}
-
-	inline public static function browserLoad(site:String) {
+	public static function browserLoad(site:String):Void {
 		#if linux
 		Sys.command('/usr/bin/xdg-open', [site]);
 		#else
@@ -111,46 +79,31 @@ class CoolUtil
 		#end
 	}
 
-	inline public static function openFolder(folder:String, absolute:Bool = false) {
+	public static function openFolder(folder:String, absolute:Bool = false):Void {
 		#if sys
-			if(!absolute) folder =  Sys.getCwd() + '$folder';
-
-			folder = folder.replace('/', '\\');
-			if(folder.endsWith('/')) folder.substr(0, folder.length - 1);
-
-			#if linux
-			var command:String = '/usr/bin/xdg-open';
-			#else
-			var command:String = 'explorer.exe';
-			#end
-			Sys.command(command, [folder]);
-			trace('$command $folder');
+		if (!absolute) {
+			folder = Sys.getCwd() + folder;
+		}
+		folder = folder.replace('/', '\\');
+		if (folder.endsWith('/')) {
+			folder.substr(0, folder.length - 1);
+		}
+		final command:String = #if linux '/usr/bin/xdg-open' #else 'explorer.exe' #end;
+		Sys.command(command, [folder]);
+		trace('$command $folder');
 		#else
-			FlxG.error("Platform is not supported for CoolUtil.openFolder");
+		FlxG.error('Platform is not supported for CoolUtil.openFolder');
 		#end
 	}
 
-	/**
-		Helper Function to Fix Save Files for Flixel 5
-
-		-- EDIT: [November 29, 2023] --
-
-		this function is used to get the save path, period.
-		since newer flixel versions are being enforced anyways.
-		@crowplexus
-	**/
 	@:access(flixel.util.FlxSave.validate)
 	inline public static function getSavePath():String {
-		final company:String = FlxG.stage.application.meta.get('company');
-		// #if (flixel < "5.0.0") return company; #else
-		return '${company}/${flixel.util.FlxSave.validate(FlxG.stage.application.meta.get('file'))}';
-		// #end
+		final meta:Dynamic = FlxG.stage.application.meta;
+		return '${meta.get('company')}/${flixel.util.FlxSave.validate(meta.get('file'))}';
 	}
 
-	public static function setTextBorderFromString(text:FlxText, border:String)
-	{
-		switch(border.toLowerCase().trim())
-		{
+	public static function setTextBorderFromString(text:FlxText, border:String):Void {
+		switch (border.toLowerCase().trim()) {
 			case 'shadow':
 				text.borderStyle = SHADOW;
 			case 'outline':
@@ -161,4 +114,5 @@ class CoolUtil
 				text.borderStyle = NONE;
 		}
 	}
+
 }
